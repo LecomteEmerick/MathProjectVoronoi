8be5e8cd7f3b3bc7e18b22b9e2fa4dacda94ad5e
Emerick Lecomte <emericklecomte@exoe.fr>
2015-11-26 18:45:17 +0100

    jarvis
    voir angle calcule

--- MathProjectVoronoi/Algorithm.cpp
+++ MathProjectVoronoi/Algorithm.cpp
@@ -1,5 +1,6 @@
 #include "Algorithm.h"
 #include <memory>
+#include <math.h>
 
 int getBottomLeftPointIndex(std::vector<Point2D> &pointCloud)
 {
@@ -27,34 +28,58 @@ int getBottomLeftPointIndex(std::vector<Point2D> &pointCloud)
 	return index;
 }
 
-int GetLeftNearestPointIndex(std::vector<Point2D> &pointCloud,int RefIndexPoint)
+float Angle(Point2D ref, Point2D point)
 {
-	float CurrentXDistance = -1;
-	Point2D tmp;
-	Point2D RefPoint = pointCloud[RefIndexPoint];
-	for (int i = 0; i < pointCloud.size(); ++i)
-	{
-		if (i != RefIndexPoint)
-		{
-			tmp = pointCloud[i];
-			float xDistance = sqrtf((tmp.x - RefPoint.x) * (tmp.x - RefPoint.x));
-			if (xDistance < CurrentXDistance)
-			{
+	Point2D projection(point.x, ref.y);
 
-			}
-		}
-	}
+	float distanceHypo = ref.Distance(point);
+	float distanceAdjacent = ref.Distance(projection);
+
+	float angle = acosf(distanceHypo / distanceAdjacent);
+
+	return acosf(distanceHypo / distanceAdjacent);
 }
 
 void Algorithm::MarcheDeJarvis(PointPool &pointPool)
 {
 	std::vector<Point2D> env;
-
-	int currentIndex = getBottomLeftPointIndex(pointPool.cloudPoint);
-	if (currentIndex == -1)
+	int savedIndex, firstIndex, currentIndex = 0;
+	firstIndex = getBottomLeftPointIndex(pointPool.cloudPoint);
+	if (firstIndex == -1)
 		return;
 
-	env.push_back(pointPool.cloudPoint[currentIndex]);
+	currentIndex = firstIndex;
+	float minAngle = 0, previousAngle, tmpAngle = 0;
 
+	Point2D previousPoint, currentPoint;
 
+	do
+	{
+		previousPoint = pointPool.cloudPoint[currentIndex];
+		previousAngle = minAngle;
+		minAngle = 360;
+		env.push_back(previousPoint);
+		for (int i = 0; i < pointPool.cloudPoint.size(); ++i)
+		{
+			if (i == currentIndex)
+			{
+				continue;
+			}
+			//
+			currentPoint = pointPool.cloudPoint[i];
+			tmpAngle = Angle(previousPoint, currentPoint);
+			//
+			if (tmpAngle - previousAngle < minAngle)
+			{
+				minAngle = tmpAngle - previousAngle;
+				savedIndex = i;
+			}
+			else if (tmpAngle - previousAngle == minAngle)
+			{
+				if (previousPoint.Distance(currentPoint) < previousPoint.Distance(pointPool.cloudPoint[savedIndex]))
+					savedIndex = i;
+			}
+		}
+
+	}while(currentIndex != firstIndex);
 }
--- MathProjectVoronoi/Point2D.h
+++ MathProjectVoronoi/Point2D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Color.h"
+#include <cmath>
 
 class Point2D
 {
@@ -12,5 +13,7 @@ public:
 					Point2D(float x, float y, Color c) : x(x), y(y), color(c) {}
 					Point2D(float x, float y) : x(x), y(y), color(0.0,0.0,0.0) {}
 					~Point2D() {}
+
+	float			Point2D::Distance(Point2D point) { return std::sqrt((point.x - this->x) * (point.x - this->x) + (point.y - this->y) * (point.y - this->y)); }
 };
 
